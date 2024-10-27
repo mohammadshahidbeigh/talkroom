6e2353f98b6a2b67cd7553916302edb7a50702dd
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-10-27 18:53:46 +0530

    recover code

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,12 @@
 // client/src/App.tsx
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Login from "./components/Auth/Login";
-import Register from "./components/Auth/Register";
-import Dashboard from "./components/Dashboard";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import ChatRoom from "./components/Chat/ChatRoom";
-import VideoRoom from "./components/Video/VideoRoom";
+import {BrowserRouter as Router} from "react-router-dom";
+import AppRoutes from "./routes";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/chat"
-          element={
-            <ProtectedRoute>
-              <ChatRoom />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/video"
-          element={
-            <ProtectedRoute>
-              <VideoRoom />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/" element={<Login />} /> {/* Add a default route */}
-      </Routes>
+      <AppRoutes />
     </Router>
   );
 };
