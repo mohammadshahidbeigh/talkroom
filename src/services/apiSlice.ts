b42954654574d4b8c5c3737a11332ec41010456b
Mohammad Shahid Beigh <85876937+mohammadshahidbeigh@users.noreply.github.com>
2024-11-22 15:55:33 +0530

    Update apiSlice.ts

--- src/services/apiSlice.ts
+++ src/services/apiSlice.ts
@@ -12,7 +12,7 @@ import type {
   RootState,
 } from "../types";
 
-const BASE_URL = "https://talk-room-server.vercel.app";
+const BASE_URL = "https://talk-room-server.vercel.app/";
 
 // Define tag types
 type TagTypes = "Messages";
